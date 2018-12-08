bd7e2619e99e0ee88b1bdbf0f2406045623eb6c6
vshcherbyna <volodymyr@shcherbyna.com>
2018-12-09 00:32:11 +0100

    fix travis build

--- src/bitboard.h
+++ src/bitboard.h
@@ -75,28 +75,5 @@ enum
 #define bit_set(number, bit) number |= 1ULL << bit
 #define bit_unset(number, bit) number &= (~(1ULL << bit))
 
-static int firstOne(bitboard bitmap)
-{
-    // De Bruijn Multiplication, see http://chessprogramming.wikispaces.com/BitScan
-    // don't use this if bitmap = 0
-
-    static const int INDEX64[64] = {
-        63,  0, 58,  1, 59, 47, 53,  2,
-        60, 39, 48, 27, 54, 33, 42,  3,
-        61, 51, 37, 40, 49, 18, 28, 20,
-        55, 30, 34, 11, 43, 14, 22,  4,
-        62, 57, 46, 52, 38, 26, 32, 41,
-        50, 36, 17, 19, 29, 10, 13, 21,
-        56, 45, 25, 31, 35, 16,  9, 12,
-        44, 24, 15,  8, 23,  7,  6,  5 };
-
-    static const bitboard DEBRUIJN64 = 0x07EDD5E59A4E28C2;
-
-    // here you would get a warming: "unary minus operator applied to unsigned type",
-    // that's intended and OK so I'll disable it
-
-    return INDEX64[((bitmap & -bitmap) * DEBRUIJN64) >> 58];
-}
-
 pasteque_namespace_end
 #endif // BITBOARD_H
--- tests/ci.py
+++ tests/ci.py
@@ -1,20 +1,20 @@
 #
-#  pastèque - a UCI chess playing engine derived from GreKo 2018.01
+#  pastÃ¨que - a UCI chess playing engine derived from GreKo 2018.01
 #
 #  Copyright (C) 2018 Volodymyr Shcherbyna <volodymyr@shcherbyna.com>
 #
-#  pastèque is free software: you can redistribute it and/or modify
+#  pastÃ¨que is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  pastèque is distributed in the hope that it will be useful,
+#  pastÃ¨que is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with pastèque.  If not, see <http://www.gnu.org/licenses/>.
+#  along with pastÃ¨que.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 #!/usr/bin/python3
